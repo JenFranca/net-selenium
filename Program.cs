aff705da82d6b77012951926240ff61bb2a3540a
Jeniffer Fretes França <jeniffer.franca@brasil317.com.br>
2024-02-01 14:55:10 -0300

    Do jeito mais dificil

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.ObjectModel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var driver = new ChromeDriver();
+
+driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
+driver.Navigate().GoToUrl("https://www.imdb.com/chart/top");
+
+var title = driver.Title;
+Console.WriteLine(title);
+
+ReadOnlyCollection<IWebElement> filmes = driver
+    .FindElement(By.ClassName("ipc-page-grid"))
+    .FindElements(By.TagName("li"));
+
+foreach (var filme in filmes)
+{
+    try
+    {
+        var name = filme.FindElement(By.TagName("h3")).Text;
+        Console.WriteLine(name);
+    }
+    catch (System.Exception)
+    {
+      
+    }
+}
+
+
+
+
+
+
+driver.Quit();
